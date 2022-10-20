52fdada396599b9b320e13f5f54a67711480698f
papariba <staraurt@gmail.com>
2022-10-20 19:24:51 +0500

    решил 1 задачу

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,3 +1,8 @@
-function sum(m, n) {
-  // ваш код...
+  function sum(m, n) {
+let results   = m+n;
+let one = sum(20, 10);
+let two = sum(12,225);
+console.log(one);
+console.log(two);
+  return results;
 }
