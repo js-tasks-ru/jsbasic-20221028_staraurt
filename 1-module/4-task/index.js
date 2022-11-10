0b16216933fc45139627ef7224569d1642408c91
papariba <staraurt@gmail.com>
2022-11-11 02:04:59 +0500

    исправил

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -2,7 +2,4 @@ function checkSpam(str) {
 
   let lowerStr = str.toLowerCase();
   return lowerStr.includes('1xbet now') || lowerStr.includes ('xxx');
-
-  alert( checkSpam ('1xBet') );
-  alert(checkSpam ('xxx') );
 }
