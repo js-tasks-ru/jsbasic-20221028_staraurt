76e5fea0fe0dd303c162ec0a51fe954f81400289
papariba <staraurt@gmail.com>
2022-11-11 01:55:46 +0500

    решил? 3 задачу 2 темы

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,20 @@
 let calculator = {
-  // ваш код
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
+
+  read() {
+    this.a = +prompt('a?', 3);
+    this.b = +prompt('b?', 5);
+  }
 };
 
+calculator.read(3, 5);
+console.log(calculator.sum());
+console.log(calculator.mul());
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
